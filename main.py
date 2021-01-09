6ab577b92b895b22c696abdf697a2d6e2bd39165
Kapilesh Pennichetty <kapileshp@pop-os.localdomain>
2021-01-09 17:36:15 -0600

    Testing gitignore

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Testing done by Kapilesh Pennichetty
+# -- Testing done by Kapilesh Pennichetty --
 from newsapi import NewsApiClient
 from flask import Flask, render_template
 newsapi = NewsApiClient(api_key='YOUR_NEWSAPI_KEY_HERE')
