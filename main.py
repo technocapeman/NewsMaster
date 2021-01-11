03d8f6ccfca2b032b6edc51debff3772ebb61696
Kapilesh Pennichetty <kapileshp@pop-os.localdomain>
2021-01-11 13:36:08 -0600

    Improved Formatting and CSS for App. Still a work in progress, though.

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-# --- Testing done by Kapilesh Pennichetty ---
+# ---------- Import Statements ----------
 from newsapi import NewsApiClient
 from flask import Flask, render_template
+import datetime
+
+
+# ---------- API and Program Prerequisites ----------
+from_date = datetime.date.today() - datetime.timedelta(30)
+to_date = datetime.date.today()
+
+# -- News API Prerequisites --
 newsapi = NewsApiClient(api_key='YOUR_NEWSAPI_KEY_HERE')
-stats = newsapi.get_everything(q='Bitcoin')
-all_articles = stats["articles"]
-article=0
+
+# -- Flask Framework Initialization --
 app = Flask(__name__)
 
+
+# ---------- Defining Functions and Variables ----------
+stats = newsapi.get_everything(q='Donald Trump', from_param=from_date, to=to_date, sort_by='relevancy', language='en')
+all_articles = stats["articles"]
+
+# ----- Webpages -----
+# -- Home Page --
 @app.route("/")
 def home():
-    return render_template('main.html', allarticles=all_articles, article=article)
+    return render_template('main.html', all_articles=all_articles)
 
+
+# ---------- Initializing Flask App ----------
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
