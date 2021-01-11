a67889aa833907b069b22629384c2c7f55b891c1
Kapilesh Pennichetty <kapilesh.gp@gmail.com>
2021-01-10 20:33:04 -0600

    Fixed For-Each Loop Issue

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 from newsapi import NewsApiClient
 from flask import Flask, render_template
 newsapi = NewsApiClient(api_key='YOUR_NEWSAPI_KEY_HERE')
-stats = newsapi.get_everything(q='Donald Trump')
+stats = newsapi.get_everything(q='Bitcoin')
 all_articles = stats["articles"]
+article=0
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return render_template('main.html', allarticles=all_articles)
+    return render_template('main.html', allarticles=all_articles, article=article)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(debug=True, threaded=True)
