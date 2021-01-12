e5bfe935cc1479bc24ad8a4b3c53db66de716f74
Kapilesh Pennichetty <kapileshp@pop-os.localdomain>
2021-01-11 18:03:42 -0600

    Improved Documentation

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-# ---------- Import Statements ----------
+# ---------- Import Statements (Kapilesh Pennichetty) ----------
 from newsapi import NewsApiClient
 from flask import Flask, render_template
-from PIL import Image
-import urllib
 import datetime
 
 
-# ---------- API and Program Prerequisites ----------
+# ---------- API and Program Prerequisites (Kapilesh Pennichetty) ----------
 from_date = datetime.date.today() - datetime.timedelta(30)
 to_date = datetime.date.today()
 
-# -- News API Prerequisites --
+# -- News API Prerequisites (Kapilesh Pennichetty) --
 newsapi = NewsApiClient(api_key='eadbfa9229d14334b96c95ccd2d733e1')
 
-# -- Flask Framework Initialization --
+# -- Flask Framework Initialization (Kapilesh Pennichetty) --
 app = Flask(__name__)
 
 
-# ---------- Defining Functions and Variables ----------
+# ---------- Defining Functions and Variables (Kapilesh Pennichetty) ----------
 stats = newsapi.get_everything(q='top-headlines', from_param=from_date, to=to_date, sort_by='publishedAt', language='en')
 all_articles = stats["articles"]
 
-# ----- Webpages -----
-# -- Home Page --
+# ----- Webpages (Kapilesh Pennichetty) -----
+# -- Home Page (Kapilesh Pennichetty) --
 @app.route("/")
 def home():
     return render_template('main.html', all_articles=all_articles)
 
 
-# ---------- Initializing Flask App ----------
+# ---------- Initializing Flask App (Kapilesh Pennichetty) ----------
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
