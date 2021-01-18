664e8cfba46d7480ab714cfdb8a1b9b22127fbca
Kapilesh Pennichetty <kapileshp@pop-os.localdomain>
2021-01-18 11:02:28 -0600

    Properly cited sources

--- main.py
+++ main.py
@@ -1,69 +1,22 @@
 # ---------- Import Statements (Kapilesh Pennichetty) ----------
-from newsapi import NewsApiClient
-from flask import Flask, render_template
-from datetime import date, timedelta
 import threading
-import schedule
 import time
+from datetime import date, timedelta
 
-# ---------- Sources ----------
-"""- Flask PWA Development Process: https://www.flaskpwa.com/
-Helped in the process of learning to develop a Progressive Web App using the Flask framework. The file at 
-static/manifest.json was derived from the manifest.json in the website.
+import schedule
+from flask import Flask, render_template
+from newsapi import NewsApiClient
 
-- W3 Schools: https://w3schools.com/
-Helped in implementing Bootstrap templates and CSS.
-
-- Stack Overflow (Used only for reading through previously asked questions): https://stackoverflow.com
-Helped in learning how to passing data from Python to HTML page: 
-https://stackoverflow.com/questions/51669102/how-to-pass-data-to-html-page-using-flask
-
-Helped in dynamically calculating 30 days into the past using Python:
-https://stackoverflow.com/questions/703907/how-would-i-compute-exactly-30-days-into-the-past-with-python-down-to-the-minut
-
-- Flask Documentation: 
-https://flask.palletsprojects.com/en/1.1.x/
-Helped in the process of learning the Flask Web Development Framework.
-
--  Jinja Documentation:
-https://jinja.palletsprojects.com/en/2.11.x/
-Helped in the process of learning how to initiate a 
-for-each loop for html attributes, and learning how to access Python functions through HTML.
-
-- How to code a WEB APP using Flask (Flask Python Tutorial for Beginners) ft ~*doggo roulette*~ by Kylie Ying:
-https://youtu.be/8q3qje9K5uU
-Helped in the process of learning how to use Flask to develop webpages and web-based applications.
-
-- Flask Tutorial #1 - How to Make Websites with Python by Tech with Tim:
-https://youtu.be/mqhxxeeTbu0
-Helped in the process of learning the basics of Flask.
-
-- Python Library for News API: https://github.com/mattlisiv/newsapi-python
-Helped implement the News API in the Progressive Web App.
-
-- Documentation for News API: https://newsapi.org/docs
-Helped in the process of learning the functions, inputs, and outputs of the News API.
-
-- Python Schedule Documentation: https://schedule.readthedocs.io/en/stable/
-Helped in scheduling tasks in Python.
-
-- Running a method as a background process in Python: 
-https://dev.to/hasansajedi/running-a-method-as-a-background-process-in-python-21li"""
 
 # ---------- API and Program Prerequisites (Kapilesh Pennichetty) ----------
-from_date = date.today() - timedelta(
-    30)  # Defining a variable to tell the API to return articles
-# as far as 30 days back using datetime module.
-to_date = date.today(
-)  # Defining a variable to tell the API to return articles until today using datetime module.
 
 # -- News API Prerequisites (Kapilesh Pennichetty) --
-newsapi = NewsApiClient(api_key='421c30c9f3514f82947322b9061b3099'
-                        )  # Registering API Key for Use and Abstracting
-# Key Away
+newsapi = NewsApiClient(api_key='421c30c9f3514f82947322b9061b3099')  # Registering API Key for Use and Abstracting
+# Key Away (Source: https://newsapi.org/docs)
 
 # -- Flask Framework Prerequisites (Kapilesh Pennichetty) --
-app = Flask(__name__)
+app = Flask(__name__)  # Source: https://flask.palletsprojects.com/en/1.1.x/
+
 
 # ---------- Defining Classes, Functions, and Variables ----------
 
@@ -88,8 +41,7 @@ trusted_news_sources = [
     "vice-news", "wired"
 ]
 
-
-def formatted_trusted_news_sources():
+def format_trusted_news_sources():
     """Formats the trusted_news_sources list for use with the News API."""
     news_outlets = ""  # Blank string to append as needed using for-each loop.
     for source in trusted_news_sources:
@@ -100,31 +52,41 @@ def formatted_trusted_news_sources():
                    0:-1]  # Removing the comma from the last news source.
     return trusted_news
 
+# ----- Fetch Articles -----
 
-# ----- Fetch Articles (Kapilesh Pennichetty) -----
+# -- Defining Article Date Range (Kapilesh Pennichetty) --
+# (Source: https://stackoverflow.com/questions/703907/how-would-i-compute-exactly-30-days-into-the-past-with-python-down-to-the-minut)
+
+from_date = date.today() - timedelta(30)  # Defining a variable to tell the API to return articles
+# as far as 30 days back using datetime module.
+
+to_date = date.today()  # Defining a variable to tell the API to return articles until today using datetime module.
+
+
+# -- Fetching and Filtering Article Data (Kapilesh Pennichetty) --
 def get_top_headlines_stats():
-    """Fetches Article Data and Metadata from the API"""
+    """Fetches Article Data and Metadata from the API (Source: https://newsapi.org/docs)"""
     top_headlines_stats = newsapi.get_top_headlines(  # Requesting data from the News API about the top headlines
-        sources=formatted_trusted_news_sources(),
+        sources=format_trusted_news_sources(),
         language='en')
     return top_headlines_stats
 
 
-def get_top_articles():  # Filters out article data from API metadata
+def get_top_articles():
+    """Filters out article data from API metadata"""
     all_top_articles = get_top_headlines_stats()[
         "articles"]  # Separating articles from the search query data
     return all_top_articles
 
-
 """
 To be used when developing search bar, filters, and sorting:
-all_articles_stats = newsapi.get_everything(sources=formatted_trusted_news_sources(),  # Requesting data from the News 
+all_articles_stats = newsapi.get_everything(sources=format_trusted_news_sources(),  # Requesting data from the News 
 # API 
 # about all articles
                                       from_param=from_date,
                                       to=to_date,
                                       language='en',
-                                      sources=formatted_trusted_news_sources(),
+                                      sources=format_trusted_news_sources(),
                                       language='en',
                                       sort_by='relevancy')
 
@@ -133,40 +95,34 @@ all_articles = all_articles_stats["articles"]  # Separating articles from the se
 Reminder: Be sure to pass the variable all_articles to the HTML page.
 """
 
-schedule.every(1).hour.do(
-    get_top_headlines_stats
-)  # Requests articles and stats from the API per set time
-schedule.every(1).hour.do(
-    get_top_articles)  # Separates articles from search query data per set time
+# -- Using Scheduler to Fetch Articles (Kapilesh Pennichetty) --
+class AutoSchedule(object):
+    """Class that Allows Scheduler to Run in a Background Thread"""
 
-
-class AutoSchedule(
-    object):  # Class that Allows Scheduler to Run in a Background Thread
     def __init__(self, interval=1):
-        """Declares and Initiates a Background Thread for fetching news articles."""
+        """Declares and Initiates a Background Thread for fetching news articles (Source:
+    https://dev.to/hasansajedi/running-a-method-as-a-background-process-in-python-21li)"""
         self.interval = interval
         thread = threading.Thread(target=self.run, args=())  # Declaring Thread
-        thread.daemon = True  # Assigning thread property of background task or daemon
+        thread.daemon = True  # Assigning thread property of background task (daemon)
         thread.start()
 
     def run(self):
-        """Runs a While loop to keep checking if there are any pending scheduled tasks."""
+        """Runs a While loop to keep checking if there are any pending scheduled tasks. (Source:
+        https://schedule.readthedocs.io/en/stable/) """
         while True:
             schedule.run_pending()
             time.sleep(self.interval)
 
 
-# ----- Webpages -----
-
+# ----- Webpages (Source: https://flask.palletsprojects.com/en/1.1.x/ and
+# https://jinja.palletsprojects.com/en/2.11.x/) -----
 
 # -- Home Page (Kapilesh Pennichetty) --
-@app.route(
-    "/"
-)  # Telling Flask that the url with "/" appended at the end should lead to the home page.
-def home():  # The home page shows trending articles.
-    return render_template(
-        'home.html', all_top_articles=get_top_articles()
-    )  # Rendering the HTML for the home page,
+@app.route("/")  # Telling Flask that the url with "/" appended at the end should lead to the home page.
+def home():
+    """Home page that shows trending articles."""
+    return render_template('home.html', all_top_articles=get_top_articles())  # Rendering the HTML for the home page,
     # passing required variables from Python to the HTML page using Jinja.
 
 
@@ -176,8 +132,15 @@ def home():  # The home page shows trending articles.
 if __name__ == "__main__":
     app.run(
         host='0.0.0.0', port=5000, debug=True, threaded=True
-    )  # Telling Flask to run the app with the constraints given.
+    )  # Telling Flask to run the app with the constraints given. (Source: https://flask.palletsprojects.com/en/1.1.x/)
 
 # ----- Other Tasks -----
-AutoSchedule(
-)  # Run methods (functions) in the AutoSchedule Class (Kapilesh Pennichetty)
+
+# -- Scheduler Tasks (Kapilesh Pennichetty) --
+
+# Adding tasks to Scheduler (Source: https://schedule.readthedocs.io/en/stable/)
+schedule.every(1).hour.do(get_top_headlines_stats)  # Requests articles and stats from the API per set time
+schedule.every(1).hour.do(get_top_articles)  # Separates articles from search query data per set time
+
+# Run methods (functions) in the AutoSchedule Class
+AutoSchedule()
