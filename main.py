ce7986c210615eca8b6f2d71b2610c86c187d24b
Kapilesh Pennichetty <kapileshp@pop-os.localdomain>
2021-01-26 10:24:21 -0600

    Fixing issue of top_headlines not found

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from threading import Thread
 from time import sleep
 
 import requests
+import schedule
 from flask import Flask, render_template
 
 
 # ---------- API and Program Prerequisites (Kapilesh Pennichetty) ----------
 
-api_key = '421c30c9f3514f82947322b9061b3099'  # Defining API Key for use with News API
+api_key = 'ee4fcdf872be45afb0adc63701a5e2e0'  # Defining API Key for use with News API
 
 app = Flask(__name__)  # Defining Flask App (Source: https://flask.palletsprojects.com/en/1.1.x/)
 
 
-# ---------- Collection Types and Function Definitions ----------
+# ---------- Functions and Data ----------
+
+# ------- Trending News (Kapilesh Pennichetty) -------
 
 # ----- List and Format Trusted News Sources (Kapilesh Pennichetty) -----
 trusted_news_sources_list = [
@@ -43,7 +46,7 @@ trusted_news_sources = ",".join(trusted_news_sources_list)  # Formatting list fo
 
 def get_top_headlines_stats():
     """Fetches Article Data and Metadata from the API (Documentation:
-    https://newsapi.org/docs/endpoints/top-headlines) """
+    https://newsapi.org/docs/endpoints/top-headlines)"""
     url = f'http://newsapi.org/v2/top-headlines?' \
           f'sources={trusted_news_sources}&' \
           f'apiKey={api_key}'
@@ -53,32 +56,54 @@ def get_top_headlines_stats():
 
 
 def fetch_top_headlines():
-    """This function runs an infinite loop to fetch top headlines every 30 minutes"""
+    """This function runs fetches top headlines when called."""
     global top_headlines
     top_headlines = get_top_headlines_stats()
+
+
+def background_fetch():
+    """This function repeatedly calls fetch_top_headlines() every 30 minutes
+     (Documentation: https://schedule.readthedocs.io/en/stable/index.html)."""
+    fetch_top_headlines()
+    schedule.every(30).minutes.do(fetch_top_headlines)
     while True:
-        sleep(30*60)  # 30 mins x 60 secs per min = 1800 seconds
-        top_headlines = get_top_headlines_stats()
+        schedule.run_pending()
+        sleep(1)
 
 
-# ----- Webpages (Documentation: https://flask.palletsprojects.com/en/1.1.x/ and
-# https://jinja.palletsprojects.com/en/2.11.x/) -----
+# ------- Weather (Sanjay Balasubramanian) -------
 
-# -- Home Page (Kapilesh Pennichetty) --
+ext_ip = requests.get('https://api.ipify.org').text  # This line was done by Kapilesh Pennichetty
+# (Reference: https://stackoverflow.com/questions/2311510/getting-a-machines-external-ip-address-with-python)
+
+# Start writing function definitions for weather here
+
+
+# ------- Webpages (Documentation: https://flask.palletsprojects.com/en/1.1.x/ and
+# https://jinja.palletsprojects.com/en/2.11.x/) -------
+
+# ----- Home Page (Kapilesh Pennichetty) -----
 @app.route("/")  # Telling Flask that the url with "/" appended at the end should lead to the home page.
 def home():
     """Home page that shows trending articles."""
     return render_template('home.html', top_articles=top_headlines)
     # Rendering the HTML for the home page, passing required variables from Python to the HTML page using Jinja.
 
+# ----- Weather Page (Kapilesh Pennichetty) -----
+@app.route("/weather")
+def weather():
+    """Page that shows weather info."""
+    return render_template('weather.html')  # Rendering the HTML for the home page, passing required variables from
+    # Python to HTML page using Jinja.
+
 
 # ---------- Main Code ----------
 
-# ----- Run repeat_top_headlines() in a background thread (Kapilesh Pennichetty) -----
+# ----- Run background_fetch() in a background thread (Kapilesh Pennichetty) -----
 # Reference: https://stackoverflow.com/questions/38254172/infinite-while-true-loop-in-the-background-python
-fetch_articles = Thread(name='background', target=fetch_top_headlines)
-fetch_articles.daemon = True
-fetch_articles.start()
+fetch_articles = Thread(name='background', target=background_fetch)  # Creating thread for background task
+fetch_articles.daemon = True  # Declaring that thread is a daemon (background task)
+fetch_articles.start()  # Initializing the thread
 
 # ----- Run Flask App (Kapilesh Pennichetty) -----
 if __name__ == "__main__":
