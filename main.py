04cd9ec65450957b27d276672868acacaeee5518
Kapilesh Pennichetty <kapilesh.gp@gmail.com>
2021-03-13 14:34:16 -0600

    trying jsonify

--- main.py
+++ main.py
@@ -203,14 +203,16 @@ def weather():
             4])  # Rendering the HTML for the home page, passing required variables from
     # Python to HTML page using Jinja.
 
+
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
-    return type(request.environ['HTTP_X_FORWARDED_FOR'])
-    """ip_info = request.environ['HTTP_X_FORWARDED_FOR']
-    if "," in request.environ['HTTP_X_FORWARDED_FOR']:
+    ip_info = jsonify(request.environ['HTTP_X_FORWARDED_FOR'])
+    if "," in ip_info:
         return ip_info[0, ip_info.index(",")]
     else:
-        return ip_info"""
+        return ip_info
+
+
 # ---------- Main Code ----------
 
 # ----- Run background_fetch() in a background thread (Kapilesh Pennichetty) -----
