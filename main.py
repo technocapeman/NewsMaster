0a8c7bc476edce31af8c50fd4809b600670debae
Kapilesh Pennichetty <kapilesh.gp@gmail.com>
2021-03-13 15:58:25 -0600

    testing ip

--- main.py
+++ main.py
@@ -207,11 +207,10 @@ def weather():
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
     ip_info = jsonify(request.environ['HTTP_X_FORWARDED_FOR'])
-    return ip_info
-    """if "," in ip_info:
-        return ip_info[0, ip_info.index(",")]
+    if "," in ip_info:
+        return ip_info[:ip_info.index(",")]
     else:
-        return ip_info"""
+        return ip_info
 
 
 # ---------- Main Code ----------
