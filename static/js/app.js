5c27dbeffc21ec0d3decc7ba21042e6cccd6de03
Kapilesh Pennichetty <kapilesh.gp@gmail.com>
2021-03-20 23:57:24 -0500

    Testing new Service Worker in Production Environment

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,19 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("sw.js")
-        .then(() => console.log("registered service worker!"));
-}
+// Credits: https://github.com/umluizlima/flask-pwa/blob/master/app/static/js/app.js
+
+(function() {
+  if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+               .then(function(registration) {
+               console.log('Service Worker Registered');
+               return registration;
+      })
+      .catch(function(err) {
+        console.error('Unable to register service worker.', err);
+      });
+      navigator.serviceWorker.ready.then(function(registration) {
+        console.log('Service Worker Ready');
+      });
+    });
+  }
+})();
