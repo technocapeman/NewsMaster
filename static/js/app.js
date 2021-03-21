c90d1f0425296a3c7ad072b53b515a9a2b663d32
Kapilesh Pennichetty <kapilesh.gp@gmail.com>
2021-03-21 10:19:41 -0500

    Added new Advanced Service Worker with Network First Strategy for HTML documents.

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,19 @@
 // Credits: https://github.com/umluizlima/flask-pwa/blob/master/app/static/js/app.js
 
 (function() {
-  if('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-               .then(function(registration) {
-               console.log('Service Worker Registered');
-               return registration;
-      })
-      .catch(function(err) {
-        console.error('Unable to register service worker.', err);
+  if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+          navigator.serviceWorker.register('/service-worker.js')
+              .then(function(registration) {
+                  console.log('Service Worker Registered');
+                  return registration;
+              })
+              .catch(function(err) {
+                  console.error('Unable to register service worker.', err);
+              });
+          navigator.serviceWorker.ready.then(function(registration) {
+              console.log('Service Worker Ready');
+          });
       });
-      navigator.serviceWorker.ready.then(function(registration) {
-        console.log('Service Worker Ready');
-      });
-    });
   }
 })();
