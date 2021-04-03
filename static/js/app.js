3f4a3c5afb1a0c8952ae9ac3f5c89898b226a21c
Kapilesh Pennichetty <kapilesh.gp@gmail.com>
2021-04-03 18:35:20 -0500

    Code cleanup and update to credits

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,13 @@
-// Credits: https://github.com/umluizlima/flask-pwa/blob/master/app/static/js/app.js
+// Credits: https://flaskpwa.com/#_serviceWorkersRegistration
 
-(function() {
-  if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-          navigator.serviceWorker.register('/service-worker.js')
-              .then(function(registration) {
-                  console.log('Service Worker Registered');
-                  return registration;
-              })
-              .catch(function(err) {
-                  console.error('Unable to register service worker.', err);
-              });
-          navigator.serviceWorker.ready.then(function(registration) {
-              console.log('Service Worker Ready');
-          });
-      });
-  }
-})();
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(function(registration) {
+        console.log('Service Worker Registered!');
+        return registration;
+    })
+    .catch(function(err) {
+        console.error('Unable to register service worker.', err);
+    });
+}
